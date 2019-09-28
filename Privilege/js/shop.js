f2796a7cb9bce5bffa574adf4d3ae9f2e42643ff
WorkForPoops <48695471+WorkForPoops@users.noreply.github.com>
2019-09-28 18:18:35 +0300

    test_number

--- Privilege/js/shop.js
+++ Privilege/js/shop.js
@@ -17,3 +17,101 @@ $(window).on('load', function () {
     $svg_anm.fadeOut();
     $preloader.delay(500).fadeOut('slow');
 });
+
+
+
+$(function () {
+$('[data-toggle="popover"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_1"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_2"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_3"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_4"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_5"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_6"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_7"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_8"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_9"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_10"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_11"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_12"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_13"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_14"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_15"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_16"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_17"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_18"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_19"]').popover()
+});
+
+$(function () {
+$('[data-toggle="popover_20"]').popover()
+});
+
+
+
+
+
+
+$(document).ready(function () {
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+});
