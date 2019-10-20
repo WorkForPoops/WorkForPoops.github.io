0a4b7510fce43dbe9c4771709272132411109ca4
WorkForPoops <48695471+WorkForPoops@users.noreply.github.com>
2019-10-20 21:13:13 +0300

    seafood

---
+++ sea_food/js/js_food.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
+
+
+
+//Preloader
+
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
