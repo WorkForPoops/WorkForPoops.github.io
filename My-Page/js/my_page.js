f27c409cb540eac29398679cc71c5cf853c2d225
vagon_straponov <Zelinov_1097@mail.ru>
2019-11-04 16:53:01 +0300

    add all files from project my page

---
+++ My-Page/js/my_page.js
@@ -0,0 +1,19 @@
+
+document.querySelector('.c-hamburger').addEventListener('click', function (e){
+	e.preventDefault();
+	if(this.classList.contains('is-active')){
+		this.classList.remove('is-active');
+		document.querySelector('#menu').classList.remove('nav-active');
+		document.body.classList.remove('body-active');
+	}
+	else{
+		this.classList.add('is-active');
+		document.querySelector('#menu').classList.add('nav-active');
+		document.body.classList.add('body-active');
+		
+	}
+})
+
+document.querySelector('.kosmonavt').onclick = function(){
+	alert('hello');
+}
