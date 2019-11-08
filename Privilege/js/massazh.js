a9a5fb2feeda7a5b2262d3083c07a5301fc53dcc
vagon_straponov <Zelinov_1097@mail.ru>
2019-11-08 19:07:15 +0300

    add all files

---
+++ Privilege/js/massazh.js
@@ -0,0 +1,31 @@
+let m_n_1 = document.querySelector('.m_n_1');
+let m_n_2 = document.querySelector('.m_n_2');
+let m_n_3 = document.querySelector('.m_n_3');
+
+m_n_1.onclick = () => {
+	m_n_1.style.color = 'red';
+	m_n_2.style.color = 'black';
+	m_n_3.style.color = 'black';
+}
+
+m_n_2.onclick = () => {
+	m_n_1.style.color = 'black';
+	m_n_2.style.color = 'red';
+	m_n_3.style.color = 'black';
+}
+
+m_n_3.onclick = () => {
+	m_n_1.style.color = 'black';
+	m_n_2.style.color = 'black';
+	m_n_3.style.color = 'red';
+}
+
+//HAMBURGER
+
+$(document).ready(function () {
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+});
+
---
+++ Privilege/js/script.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function() {
+
+	
+	//E-mail Ajax Send
+
+	$("form").submit(function() { //Change
+		
+		var check_b = prompt('Проверка на безопасность. Какое число идет после 1?',0);
+		
+		if(check_b.value != 2){
+			alert('Ошибка');
+		}else if(check_b.value == 2){
+			var th = $(this);
+
+			$.ajax({
+
+				type: "POST",
+
+				url: "mail.php", //Change
+
+				data: th.serialize()
+
+			}).done(function() {
+
+				swal("Отлично!", "Ваше письмо отправлено", "success");
+
+				setTimeout(function() {
+
+					// Done Functions
+
+					th.trigger("reset");
+
+				}, 1000);
+
+			});	
+		}
+
+		return false;
+
+	});
+
+
+
+});
+
+
+//HAMBURGER
+$(document).ready(function () {
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+});
